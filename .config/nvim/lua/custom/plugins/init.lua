c25b4b18b682512b310f2d6a6abca98a3a9cdebc
Thomas Jack <tom@tomjack.ca>
2025-10-19 20:11:46 -0400

    Update Plugin Verions

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -9,4 +9,44 @@ return {
     },
     lazy = false,
   },
+  {
+    'greggh/claude-code.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('claude-code').setup()
+    end,
+  },
+  {
+    'elixir-tools/elixir-tools.nvim',
+    version = '*',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local elixir = require 'elixir'
+      local elixirls = require 'elixir.elixirls'
+
+      elixir.setup {
+        nextls = {
+          enable = true,
+          init_options = {
+            experimental = {
+              completions = {
+                enable = true, -- Enable auto-completions
+              },
+            },
+          },
+        },
+        elixirls = {
+          enable = false,
+        },
+        projectionist = {
+          enable = false,
+        },
+      }
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
 }
